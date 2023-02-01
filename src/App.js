5e5a11b7969c0dd9054b214dec4f3f5d634ce14a
Park Na Yeon <within197@naver.com>
2023-02-01 16:53:47 +0900

    [FEAT] Add Susepense and lazy for code spliting

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
-import React from 'react'
-import { Switch, Route } from 'react-router-dom'
-import './App.css'
-import ListPage from './pages/ListPage/index'
-import ViewPage from './pages/ViewPage/index'
+import React, { Suspense, lazy } from 'react';
+import { Switch, Route } from 'react-router-dom';
+import './App.css';
+// import ListPage from './pages/ListPage/index';
+// import ViewPage from './pages/ViewPage/index';
+
+// 동적 import
+const ListPage = lazy(() => import('./pages/ListPage/index'));
+const ViewPage = lazy(() => import('./pages/ViewPage/index'));
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <Switch>
-        <Route path="/" component={ListPage} exact />
-        <Route path="/view/:id" component={ViewPage} exact />
+        <Suspense fallback={<div>loading...</div>}>
+          <Route path='/' component={ListPage} exact />
+          <Route path='/view/:id' component={ViewPage} exact />
+        </Suspense>
       </Switch>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
